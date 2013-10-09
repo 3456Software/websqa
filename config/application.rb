30398bc96567c828d609441bd8f0026410fc9b27
Griffin Yourick <gryphon92@gmail.com>
2013-10-09 17:16:37 -0400

    Fix html 5 shim

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Websqa
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.precompile += ['html5shiv.min.js', 'respond.min.js']
   end
 end
