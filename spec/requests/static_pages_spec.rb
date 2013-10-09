357879f73f0d4b058af9fb733a31aa5f7aef12fd
Griffin Yourick <gryphon92@gmail.com>
2013-10-09 15:24:25 -0400

    Add more static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,4 +17,40 @@ describe 'Static Pages' do
     it_should_behave_like 'all_static_pages'
     it { should_not have_title(' | Home') }
   end
+
+  describe 'Help page' do
+    before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like 'all_static_pages'
+  end
+
+  describe 'About page' do
+    before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like 'all_static_pages'
+  end
+
+  describe 'Contact page' do
+    before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact'}
+
+    it_should_behave_like 'all_static_pages'
+  end
+
+  it 'should have all the right links' do
+    visit root_path
+    click_link 'About'
+    expect(page).to have_title(full_title('About'))
+    click_link 'Help'
+    expect(page).to have_title(full_title('Help'))
+    click_link 'Contact'
+    expect(page).to have_title(full_title('Contact'))
+    click_link 'WebSQA'
+    expect(page).to have_title(full_title(''))
+  end
 end
