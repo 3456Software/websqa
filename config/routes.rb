9c6c03d31827fc2db1cbe6b5b0b9b3b8c46dad85
Griffin Yourick <gryphon92@gmail.com>
2013-10-16 02:09:23 -0400

    Clean up css, fix homepage link text

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Websqa::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
 
   # for demo pages
   match '/signin',  to: 'demo_pages#signin',    via: 'get'
-  match '/signup',  to: 'users#new',            via: 'get'
 end
