60c2fa6c8097be0c85665478bb786d576b59f672
Griffin Yourick <gryphon92@gmail.com>
2013-10-16 21:56:04 -0400

    Implement sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Websqa::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
-
-  # for demo pages
-  match '/signin',  to: 'demo_pages#signin',    via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
